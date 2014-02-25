acc863d0f74e6d4d52883f642b62beb021e6f53d
Tonn Rüter <truter@esrf.eu>
2014-02-25 21:11:23 +0100

    Added flags and insert/remove functionality to QDirListModel

--- RixsTool/datahandling.py
+++ RixsTool/datahandling.py
@@ -25,23 +25,177 @@
 # is a problem for you.
 #############################################################################*/
 __author__ = "Tonn Rueter - ESRF Data Analysis Unit"
+from RixsTool.RixsUtils import unique as RixsUtilsUnique
 from RixsTool.calculations import *
+import RixsTool.io as IO
 from PyMca import PyMcaQt as qt
+from os.path import split as OsPathSplit
+from os import access as OsAccess
+from os import R_OK as OS_R_OK
+from os import listdir as OsListDir
+from os.path import isfile as OsPathIsFile, join as OsPathJoin
+from os.path import normpath as OsPathNormpath
+
 
 DEBUG == 1
+
+class RixsProject(qt.QObject):
+    __IMAGES = 'images'
+    __SPECTRA = 'spectra'
+    __ANALYSIS = 'analysis'
+
+    __SPEC_TYPE = 'spec'
+    __EDF_TYPE = 'edf'
+    __RAW_TYPE = 'raw'
+
+    defaultWorkingDirectory = 'C:\\Users\\tonn\\lab\\rixs\\Images'
+
+    def __init__(self, workingDir=''):
+        super(RixsProject, self).__init__()
+        #
+        # Set working directory
+        #
+        if len(workingDir) <= 0:
+            self.workingDir = qt.QDir(self.defaultWorkingDirectory)
+        else:
+            self.workingDir = qt.QDir(workingDir)
+
+        #
+        # Spec readers
+        #
+        # TODO: Have readers for specfiles..
+        self.specReaders = {
+            self.__SPEC_TYPE : None
+        }
+
+        #
+        # Image readers
+        #
+        self.imageReaders = {
+            self.__EDF_TYPE : IO.EdfInputReader(),
+            self.__RAW_TYPE : IO.RawTextInputReader()
+        }
+
+    def readImage(self, filename, fileType):
+        try:
+            reader = self.imageReaders[fileType]
+        except KeyError:
+            return False
+        reader.append(filename)
+        return True
+
+    def readImages(self, filelist, fileType):
+        try:
+            reader = self.imageReaders[fileType]
+        except KeyError:
+            return False
+        reader.refresh(filelist)
+        return True
+
+    def getImage(self, reader, key, index):
+        #if key not in reader.getFileList():
+        #    raise IndexError("RixsProject.getImage -- Key '%s' not found "%key)
+        return reader['Images']
+
+    def addFileInfoList(self, fileInfoList):
+        if DEBUG == 1:
+            print('RixsProject.addFileInfoList -- received fileInfoList (len: %d)'%\
+                  len(fileInfoList))
+        for info in fileInfoList:
+            name = OsPathNormpath(info.completeSuffix())
+            suffix = str(info.completeSuffix())
+            if suffix.lower not in self.imageReaders.keys():
+                raise ValueError("Unknown filetype: '%s'"%suffix)
+            reader = self.imageReaders[suffix]
+            reader.append(name)
+            print(reader)
+
+    def getFileNames(self):
+        llist = [reader.getFileList() for reader in self.imageReaders.values()]
+        return sum(llist, [])
 
 class QDirListModel(qt.QAbstractListModel):
     def __init__(self, parent=None):
         super(QDirListModel, self).__init__(parent)
-        #self.__directoryList = []
-        self.__directoryList = [qt.QDir('C:\\Users\\tonn\\lab\\rixs\\Images'),
-                                qt.QDir('C:\\Users\\tonn\\lab\\rixs')]
+        self.__directoryList = []
 
     def __getitem__(self, idx):
+        """
+        :param idx: Return idx-th element in the model
+        :type idx: int
+        """
         return self.__directoryList[idx]
 
-    def rowCount(self, modelIndex):
+    def flags(self, modelIndex):
+        if modelIndex.isValid():
+            return qt.Qt.ItemIsSelectable | qt.Qt.ItemIsEditable
+        else:
+            if DEBUG == 1:
+                print('QDirListModel.flags -- received invalid modelIndex')
+            return 0
+
+    def __len__(self):
         return len(self.__directoryList)
+
+    def rowCount(self, modelIndex = qt.QModelIndex()):
+        return len(self.__directoryList)
+
+    def insertDirs(self, row, directoryList):
+        """
+        :param row: Determines after which row the items are inserted
+        :type row: int
+        :param directoryList: Carries the new legend information
+        :type directoryList: list of either strings or QDirs
+        """
+        modelIndex = self.createIndex(row,0)
+        count = len(directoryList)
+        qt.QAbstractListModel.beginInsertRows(self,
+                                              modelIndex,
+                                              row,
+                                              row+count)
+        head = self.__directoryList[0:row]
+        tail = self.__directoryList[row:]
+        new  = [qt.QDir()] * count
+        for idx, elem in enumerate(directoryList):
+            if isinstance(elem, str):
+                newDir = qt.QDir(elem)
+            elif isinstance(elem, qt.QDir):
+                # Call copy ctor
+                newDir = qt.QDir(elem)
+            else:
+                if DEBUG == 1:
+                    print('QDirListModel.insertDirs -- Element %d: Neither instance of str nor QDir'%idx)
+                continue
+            new[idx] = newDir
+        self.__directoryList = head + new + tail
+        # Reduce self.__directoryList to unique elements..
+        RixsUtilsUnique(self.__directoryList, 'absolutePath')
+        qt.QAbstractListModel.endInsertRows(self)
+        return True
+
+    def insertRows(self, row, count, modelIndex = qt.QModelIndex()):
+        raise NotImplementedError('Use LegendModel.insertLegendList instead')
+
+    def removeDirs(self, row, count, modelIndex = qt.QModelIndex()):
+        length = len(self.__directoryList)
+        if length == 0:
+            # Nothing to do..
+            return True
+        if row < 0 or row >= length:
+            raise IndexError('Index out of range -- '
+                            +'idx: %d, len: %d'%(row, length))
+        if count == 0:
+            return False
+        qt.QAbstractListModel.beginRemoveRows(self,
+                                              modelIndex,
+                                              row,
+                                              row+count)
+        del(self.__directoryList[row:row+count])
+        qt.QAbstractListModel.endRemoveRows(self)
+        return True
+
+    def removeRows(self, row, count, modelIndex = qt.QModelIndex()):
+        raise NotImplementedError('QDirListModel.removeRows -- Not implemented, use QDirListModel.removeDirs instead')
 
     def data(self, modelIndex, role):
         if modelIndex.isValid():
@@ -86,7 +240,6 @@ class DataHandler(qt.QObject):
                 stats.basics(image)
         return True
 
-
 class OpDispatcher(qt.QObject):
     sigOpFinished = qt.pyqtSignal(object)
 
@@ -102,3 +255,40 @@ class DummyNotifiyer(qt.QObject):
         obj = self.sender()
         print('Signal received:',ddict)
         #print('%s : %s'%(str(obj),str(kwargs)))
+
+def unitTest_QDirListModel():
+    inp = ['foo/dir','bar\\dir','baz']
+    listModel = QDirListModel()
+    listModel.insertDirs(0, inp)
+
+    print('datahandling.unitTest_QDirListModel -- Input string list:', str(inp))
+
+    first = (len(listModel) == 3) and (listModel.rowCount() == 3)
+    second, third = True, True
+    for idx in range(len(listModel)):
+        modelIndex = listModel.createIndex(idx, 0)
+        displayRole = listModel.data(modelIndex, qt.Qt.DisplayRole)
+        flag = listModel.flags(modelIndex)
+        qdir = listModel[idx]
+
+        second &= isinstance(displayRole, str)
+        third  &= isinstance(qdir, qt.QDir)
+
+        print('\t%d: %s\t%s\t%s\t%s'%\
+              (idx, str(displayRole), type(displayRole), int(flag), str(qdir)))
+
+    if first and second and third:
+        print('datahandling.unitTest_QDirListModel -- Success')
+        return True
+    else:
+        print('datahandling.unitTest_QDirListModel -- Failure')
+        return False
+
+
+if __name__ == '__main__':
+    unitTest_QDirListModel()
+    #rixsImageDir = 'C:\\Users\\tonn\\lab\\rixs\\Images'
+    #proj = RixsProject()
+    #edfImageList = [OsPathJoin(rixsImageDir,fn) for fn in OsListDir(rixsImageDir)\
+    #             if OsPathIsFile(OsPathJoin(rixsImageDir,fn))][1:]
+    #proj.readImages(edfImageList, 'edf')
