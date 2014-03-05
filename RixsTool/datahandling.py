c4c1535fbbd3eff045bb860450e941d9d116deb9
Tonn Rüter <truter@esrf.eu>
2014-03-05 20:59:49 +0100

    Moved DataItem out of DataHandling

--- RixsTool/datahandling.py
+++ RixsTool/datahandling.py
@@ -30,67 +30,22 @@ import RixsTool.io as IO
 from os.path import normpath as OsPathNormpath
 from os.path import splitext as OsPathSplitext
 from os import walk as OsWalk
-from uuid import uuid4
 
 DEBUG = 1
 
-class DataItem(object):
-    __doc__ = """Generic class to contain data"""
-    interpretation = 'Dataset'
-
-    def __init__(self, key, header, array, fileLocation):
-        super(DataItem, self).__init__()
-        self.key = key
-        self.header = header
-        self.array = array
-        self.fileLocation = fileLocation
-        self.__identifier = uuid4()
-
-    def key(self):
-        return self.key
-
-    def description(self):
-        return self.interpretation
-
-    def shape(self):
-        return self.array.shape
-
-    def dtype(self):
-        return self.array.dtype
-
-    def getID(self):
-        return self.__identifier
-
-    def hdf5Dump(self):
-        raise NotImplementedError('DataItem.hdfDump -- to be implemented here?')
-
-class ScanItem(DataItem):
-    __doc__ = """Class to contain data in multiple 1D numpy arrays"""
-    interpretation = 'Scan'
-    pass
-
-class SpecItem(DataItem):
-    __doc__ = """Class to contain data in 1D numpy array"""
-    interpretation = 'Spec'
-    pass
-
-class ImageItem(DataItem):
-    __doc__ = """Class to contain data in 2D numpy array"""
-    interpretation = 'Image'
-    pass
-
-class StackItem(DataItem):
-    __doc__ = """Class to contain data in 3D numpy array"""
-    interpretation = 'Stack'
-    pass
-
 class ItemContainer(object):
-    def __init__(self, item=None, parent=None):
+    def __init__(self, item=None, parent=None, label=None):
         #self._data = data if data is not None else [] # Dict or OrderedDict here?
         self._item = item
         self._data = ['key','description', 'shape', 'dtype']
         self.parent = parent
         self.children = []
+        if label:
+            self.label = label
+        elif item:
+            self.label = item.key
+        else:
+            self.label = ''
 
     #
     # Methods acting on ItemContainer.children or ItemContainer.parent
@@ -194,9 +149,9 @@ class ItemContainer(object):
         """
         # Modifies self.children
         # Insert ItemContainer instances
-        if (pos < 0) or (pos > len(self.children)):
+        if (pos < -1) or (pos > len(self.children)):
             return False
-        if False in [isinstance(child, ItemContainer) for child in container]:
+        if False in [isinstance(child, ItemContainer) for child in containerList]:
             return False
 
         head = self.children[0:pos]
@@ -254,27 +209,16 @@ class RixsProject(object):
         }
 
         #
-        # Groups managment
+        # Data tree
         #
-        self.groups = {
-            'images': [[]]
-        }
+        self.projectRoot = ItemContainer()
+        self.projectRoot.addChildren(
+            [ItemContainer(parent=self.projectRoot, label=key)\
+             for key in ['Spectra','Images','Stacks']])
+        print('projectRoot.childCount:',self.projectRoot.childCount())
 
-    def groupCount(self, dataType):
-        if dataType not in ['images']:
-            raise KeyError("RixsProject.groupCount -- unknown type '%s'"%str(dataType))
-        return len(self.groups[dataType])
-
-    def dataCount(self, dataType):
-        if dataType not in ['images']:
-            raise KeyError("RixsProject.groupCount -- unknown type '%s'"%str(dataType))
-        return sum([len(group) for group in self.groups[dataType]], 0)
-
-    def imagify(self, llist):
-        """
-        :param llist: List of 2D numpy arrays
-        """
-        raise NotImplementedError('RixsProject.imagify -- ...to be implemented')
+    def groupCount(self):
+        return self.projectRoot.childCount()
 
     def image(self, key, imageType):
         """
@@ -285,7 +229,8 @@ class RixsProject(object):
 
         Image -> 2D numpy array
 
-        Returns image from the data structure
+        :returns: Image from the input reader
+        :rtype: ImageItem
         """
         reader = self.imageReaders.get(imageType, None)
         if reader is None:
@@ -295,36 +240,29 @@ class RixsProject(object):
         if key not in keys:
             raise KeyError("RixsProject.image -- Key '%s' not found"%key)
         idx = keys.index(key)
-        key, \
-        header, \
-        image, \
-        numImages, \
-        fileLoc = reader[idx]
-        if numImages > 1:
-            print("RixsProject.image -- Image '%s' is a stack!"%key)
-        return image
+        return reader[idx]
 
-    def readImage(self, filename, fileType):
+    def addImage(self, image, node=None):
+        if not node:
+            node = self.projectRoot.children[1] # TODO: Find better way to access children
+        child = ItemContainer(
+            item=image,
+            parent=node
+        )
+        node.addChildren([child])
+
+    def readImage(self, filename, imageType):
         try:
-            reader = self.imageReaders[fileType]
+            reader = self.imageReaders.get(imageType, None)
         except KeyError:
-            raise ValueError("RixsProject.readImages -- Unknown image Type '%s'"%fileType)
+            raise ValueError("RixsProject.readImages -- Unknown image Type '%s'"%imageType)
+        keys = reader['FileLocations']
         reader.refresh([filename])
-        if fileType in [self.EDF_TYPE, self.RAW_TYPE]:
-            group = self.groups['images']
-        else:
-            raise ValueError("RixsProject.readImages -- Unknown image Type '%s'"%fileType)
-        #key = reader.keys()[-1]
-        key, \
-        header, \
-        image, \
-        numImages, \
-        fileLoc = reader[-1]
-        shapeStr = '*'.join([str(dim) for dim in image.shape])
-        entry = (key, shapeStr, str(numImages), fileLoc)
-        if entry not in sum(group, []):
-            #group[0].append(key)
-            group[0] += [entry]
+        if filename not in keys:
+            keys = reader['FileLocations']
+            print(keys)
+            idx = keys.index(filename)
+            self.addImage(reader[idx])
         return True
 
     def stack(self, key, index):
@@ -370,7 +308,6 @@ def unitTest_RixsProject():
                 print('Found edf-File:')
                 project.readImage(filename, project.EDF_TYPE)
                 print(type(project.image(file, project.EDF_TYPE)))
-    print('RixsProject.groups:\n\t',str(project.groups))
 
 if __name__ == '__main__':
     #unitTest_QDirListModel()
