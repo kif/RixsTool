eb44683682f3ba2331c5baf9b46a7a92f464d888
Tonn Ruter <truter@esrf.eu>
2014-03-21 17:41:07 +0100

    Implemented auto add functionality

--- RixsTool/widgets/FileSystemBrowser.py
+++ RixsTool/widgets/FileSystemBrowser.py
@@ -26,7 +26,7 @@
 #############################################################################*/
 __author__ = "Tonn Rueter - ESRF Data Analysis Unit"
 #
-# Imports for GUI
+# IMPORTS FOR GUI
 #
 from PyMca import PyMcaQt as qt
 from PyQt4 import uic
@@ -42,23 +42,34 @@ from RixsTool.Utils import unique as RixsUtilsUnique
 from RixsTool.ContextMenu import FileContextMenu, AddFilesAction
 from RixsTool.Models import QDirListModel
 
+#
+# IMPORTS FROM OS: General file system browsing
+#
+from os import listdir as OsListDir
+from os.path import isdir as OsPathIsDir
+from os.path import isfile as OsPathIsFile
+from os.path import join as OsPathJoin
+
 DEBUG = 1
 
 
 class DirTree(qt.QTreeView):
+    filesChangedSignal = qt.pyqtSignal(object)
+
     def __init__(self, parent):
         qt.QTreeView.__init__(self, parent)
 
         #
         # Model is qt.QFileSystemModel()
         #
-        self.setModel(qt.QFileSystemModel())
+        fsModel = qt.QFileSystemModel()
+        self.setModel(fsModel)
 
         #
         # Set up monitor for the file system
         #
         self.watcher = qt.QFileSystemWatcher()
-        self.watcher.fileChanged.connect(self.fileChanged)
+        self.watcher.directoryChanged.connect(self.handleDirectoryChangedSignal)
 
         #
         # Context menu
@@ -69,28 +80,107 @@ class DirTree(qt.QTreeView):
         else:
             self.callback = None
 
-    def fileChanged(self, **kw):
-        # TODO: Implement auto update
-        print('DirTree.fileChanged -- path:', kw)
+        #
+        # List to keep track of files and avoid unnecessary
+        #
+        self.fileList = []
 
-    def updatePath(self, path):
-        print('DirTree.updatePath -- called')
-        model = self.model()
-        model.setRootPath(path)
-        newRoot = model.index(path)
+    def handleDirectoryChangedSignal(self, path):
+        """
+        :param str path: Path to directory in which a file was added, deleted or renamed
+
+        Emits filesChangedSignal with list of fully qualified file paths (i.e. path + file name).
+        """
+        path = str(path)
+        tmpFileList = filter(OsPathIsFile, [OsPathJoin(path, directory) for directory in OsListDir(path)])
+
+        #
+        # Check for the difference between the current contents of path and the former ones
+        #
+        newFiles = [fileName for fileName in tmpFileList if fileName not in self.fileList]
+        if DEBUG >= 1:
+            print("DirTree.handleDirectoryChangedSignal -- path: '%s' (Files changes: %d)" % (str(path), len(newFiles)))
+            for fileName in newFiles:
+                print('\t%s' % str(fileName))
+        if len(newFiles) < 1:
+            return
+
+        #
+        # Update self.fileList
+        #
+        self.fileList += newFiles
+
+        #
+        # Emit signal
+        #
+        self.filesChangedSignal.emit(newFiles)
+
+    def setRootDirectory(self, path):
+        """
+        :param str path: Root directory
+
+        Sets the models root directory. Removes all directories from the views :py:class:`QFileSystemWatcher`
+        """
+        #
+        # Set model to new root path
+        #
+        fsModel = self.model()
+        fsModel.setRootPath(path)
+        newRoot = fsModel.index(path)
         self.setRootIndex(newRoot)
+
+        #
+        # Remove all directories monitered by the watcher
+        #
         watcherDirs = self.watcher.directories()
         if len(watcherDirs) > 0:
             self.watcher.removePaths(watcherDirs)
-        print('DirTree.updatePath -- watcherDirs:\n\t%s' % str(watcherDirs))
-        self.watcher.addPath(path)
+
+    def setWorkingDirectory(self, path):
+        """
+        :param str path: Working directory
+
+        Sets a working directory in the :py:class:`DirTree`. The working directory differs from the roots
+         directory in that the views :py:class:`QFileSystemWatcher` monitors the directory and the immediate
+         subdirectories for changes.
+
+        **Note from the Qt-Manual:**
+        For every monitored directory, system resources are consumend.
+        """
+        self.setRootDirectory(path)
+
+        #
+        # Look for subdirectories in path and let them be monitored. Update self.fileList
+        #
+        lsDir = [OsPathJoin(path, item) for item in OsListDir(path)]
+        directoryList = [path] + filter(OsPathIsDir, lsDir)
+        self.watcher.addPaths(directoryList)
+
+        #
+        # Keep track of files in the directory
+        #
+        self.fileList = filter(OsPathIsFile, lsDir)
+        for directory in directoryList:
+            lsSubDir = [OsPathJoin(directory, item) for item in OsListDir(directory)]
+            self.fileList += filter(OsPathIsFile, lsSubDir)
+
+        if DEBUG >= 1:
+            print('DirTree.setWorkingDirectory -- watcherDirs (%d):' % len(self.watcher.directories()))
+            for directory in self.watcher.directories():
+                print("\t'%s'" % str(directory))
+            print('DirTree.setWorkingDirectory -- self.fileList (%d):' % len(self.fileList))
+            for filename in self.fileList:
+                print("\t'%s'" % str(filename))
 
     def contextMenuEvent(self, event):
-        print('DirTree.contextMenuEvent -- called')
+        """
+        :param QContextMenuEvent event: Request for context menu
+
+        Shows a custom context menu of type :py:class:`RixsTool.widgets.ContextMenu.FileContextMenu`.
+        """
         modelIndexList = self.selectedIndexes()
         RixsUtilsUnique(modelIndexList, "row")
         #modelIndexList = [self.indexAt(event.pos())]
-        print('Length modelIndexList:', len(modelIndexList))
 
         model = self.model()
         fileInfoList = [model.fileInfo(idx) for idx in modelIndexList]
@@ -112,6 +202,13 @@ class DirTree(qt.QTreeView):
         return
 
     def setModel(self, fsModel):
+        """
+        :param QFileSystemModel fsModel: Model to be visualized by the view
+
+        Reimplements :py:function:`qt.QTreeView.setModel` to ensure the usage of a :py:class:`qt.QFileSystemModel`.
+
+        :raises ValueError: if model is not of type QFileSystemModel
+        """
         if not isinstance(fsModel, qt.QFileSystemModel):
             raise ValueError('DirTree.setModel -- provided model must be of type QFileSystemModel')
         else:
@@ -119,6 +216,14 @@ class DirTree(qt.QTreeView):
 
 
 class FileSystemBrowser(qt.QWidget):
+    __doc__ = """Widget contains a view to browse a file system, a custom context menu to perform actions
+     on selected files. Working directory functionality is offered to switch between folders in the file
+      system. All changes in the working directory and the next level of subdirectories is monitored by a
+      QFileSystemMonitor.
+
+    **Signals**
+        * addSignal: Emits a qt.QFileInfoList
+    """
     addSignal = qt.pyqtSignal(object)
 
     def __init__(self, parent=None):
@@ -141,17 +246,17 @@ class FileSystemBrowser(qt.QWidget):
         #
         # Connect
         #
-        self.fsView.watcher.fileChanged.connect(self.handleFileChangedSignal)
-        #self.fsView.watcher.directoryChanged.connect(self.handleFileChanged)
+        self.fsView.filesChangedSignal.connect(self.handleFilesChanged)
         self.workingDirCB.currentIndexChanged.connect(self.handleWorkingDirectoryChanged)
 
         self.addDirButton.clicked[()].connect(self.addDir)
         self.closeDirButton.clicked[()].connect(self.closeDir)
 
         # TODO: change startDir = qt.QDir.home()
-        startDir = qt.QDir.home()
-        #startDir = qt.QDir('/home/truter/lab/mock_folder/')
-        self.fsView.updatePath(startDir.absolutePath())
+        #startDir = qt.QDir.home()
+        startDir = qt.QDir('/home/truter/lab/mock_folder/')
+        self.addDir(startDir.absolutePath())
+        #self.fsView.setWorkingDirectory(startDir.absolutePath())
 
     #
     # Handler functions for various user interaction:
@@ -159,19 +264,46 @@ class FileSystemBrowser(qt.QWidget):
     #   directoryChanged
     #   currentIndexChanged
     #
-    def handleFileChangedSignal(self, **kw):
-        print('FileSystemBrowser.handleFileChangedSignal -- kw: %s' % str(kw))
-        pass
+
+    def handleFilesChanged(self, fileList):
+        """
+        :param list fileList: Contains file paths to modified files
+
+        Emits addSignal with the a :py:class:`qt.QFileInfoList` as parameter
+        """
+        #
+        # Check if files shall be automatically added to the project
+        #
+        if self.autoAddCheckBox.checkState() == qt.Qt.Unchecked:
+            print('FileSystemBrowser.handleFilesChanged -- AutoAdd off')
+            return
+
+        #
+        # Create a file info list
+        #
+        print('FileSystemBrowser.handleFilesChanged -- \n\tfileList: %s' % str(fileList))
+        fileInfoList = [qt.QFileInfo(path) for path in fileList]
+        self.addSignal.emit(fileInfoList)
 
     def handleWorkingDirectoryChanged(self, **kw):
+        print('FileSystemBrowser.handleWorkingDirectoryChanged -- kw:\n\t%s' % str(kw))
         print(kw)
         pass
 
-    def handleContextMenuAction(self, obj, param=None):
-        # Change to treeCallback and except qfileinfos directly
+    def handleContextMenuAction(self, action, param=None):
+        """
+        :param QAction action: Action from the context menu
+        :param dict param: Parameters that might accompany a certain action. Default: None
+
+        Based on the type of action, the function decides how to react:
+
+         :py:class:`RixsTool.ContextMenu`
+            Looks in params for key 'fileInfoList' and emits addSignal with said list of QFileInfo objects
+            as elements.
+        """
         if not param:
             param = {}
-        if isinstance(obj, AddFilesAction):
+        if isinstance(action, AddFilesAction):
             fileInfoList = param.get('fileInfoList', None)
             if fileInfoList:
                 self.addSignal.emit(fileInfoList)
@@ -180,7 +312,16 @@ class FileSystemBrowser(qt.QWidget):
     #
     # Adding/Removing working directories
     #
+
     def closeDir(self, safeClose=True):
+        """
+        :param bool safeClose: Prompts user to confirm closing of working directory. Default: True
+
+        Removes the working directory currently selected in the drop down menu.
+        """
+        #
+        # Prompt warning
+        #
         if safeClose:
             msg = qt.QMessageBox()
             msg.setIcon(qt.QMessageBox.Warning)
@@ -191,11 +332,13 @@ class FileSystemBrowser(qt.QWidget):
                 if DEBUG == 1:
                     print('FileSystemBrowser.closeDir -- Abort')
                 return
+
+        #
+        # Inteact with QDirListModel
+        #
         currentIdx = self.workingDirCB.currentIndex()
-        print('currentIdx:', currentIdx)
         model = self.workingDirCB.model()
-        print('model.rowCount:',model.rowCount())
-        print('len(model):',len(model))
+
         if model.rowCount() > 0:
             model.removeDirs(row=currentIdx,
                              count=1)
@@ -205,10 +348,15 @@ class FileSystemBrowser(qt.QWidget):
         else:
             self.workingDirCB.setCurrentIndex(0)
 
+        #
+        # Reset the DirTree to the users home directory
+        #
+        homeDir = qt.QDir.home()
+        self.fsView.setRootDirectory(homeDir)
+
     def addDir(self, path=None):
         """
-        :param path: New directory to be added
-        :type path: str
+        :param str path: New directory to be added
 
         Path is added to working directory combo box and set as new root directory in the view.
         """
@@ -222,70 +370,32 @@ class FileSystemBrowser(qt.QWidget):
                                                        options=qt.QFileDialog.ShowDirsOnly)
         path = str(path)
         if len(path) <= 0:
-            if DEBUG == 1:
+            if DEBUG >= 1:
                 print('FileSystemBrowser.addDir -- Received empty path. Return.')
             return
-
+        if DEBUG >= 1:
+            print("FileSystemBrowser.addDir -- Inserting dir '%s'" % path)
         newIdx = self.workingDirCB.currentIndex() + 1
         model = self.workingDirCB.model()
         model.insertDirs(newIdx, [path])
         # Update ComboBox and TreeView
         self.workingDirCB.setCurrentIndex(newIdx)
-        self.fsView.updatePath(path)
+        self.fsView.setWorkingDirectory(path)
         if not self.closeDirButton.isEnabled():
             self.closeDirButton.setEnabled(True)
-
-    #
-    # File selection
-    #
-    def getSelectedFiles(self):
-        # QTreeView.selectedIndexes returns list
-        # [Row1Col1, Row1Col2, ..., Row2Col1, ...].
-        # Hence, there is redundant information
-        modelIdxList = self.fsView.selectedIndexes()
-        # Reduce modelIdxList to unique row numbers
-        seen = {}
-        insertPos = 0
-        for item in modelIdxList:
-            if item.row() not in seen:
-                seen[item.row()] = True
-                modelIdxList[insertPos] = item
-                insertPos += 1
-        del modelIdxList[insertPos:]
-        # Get QFileInfo from model indexes
-        fsModel = self.fsView.model()
-        infoList = [fsModel.fileInfo(idx) for idx in modelIdxList]
-        if DEBUG == 1:
-            for elem in infoList:
-                print('\t', elem.absoluteFilePath())
-        return infoList
-
-    def addFiles(self):
-        # TODO: remove me
-        current = self.getSelectedFiles()
-        self.addSignal.emit(current)
-
-    def _handleWorkingDirectoryChanged(self, elem):
-        if isinstance(elem, int):
-            dirModel = self.workingDirCB.model()
-            if len(dirModel) > 0:
-                qdir = dirModel[elem]
-            else:
-                qdir = qt.QDir.home()
-        else:
-            qdir = qt.QDir(elem)
-        self.fsView.updatePath(qdir.path())
 
 
 class DummyNotifier(qt.QObject):
     @staticmethod
     def signalReceived(**kw):
-        print('DummyNotifier.signal received -- kw:\n', str(kw))
+        print('DummyNotifier.signal received -- kw: %s' % str(kw))
 
 
 def unitTest_FileSystemBrowser():
     app = qt.QApplication([])
+
     browser = FileSystemBrowser()
+    browser.addSignal.connect(DummyNotifier.signalReceived)
     browser.show()
     app.exec_()
 
